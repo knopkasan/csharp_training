e3b6e5f541503a55e3d40292dd14effaf975df48
knopkasan <knopka-san@yandex.ru>
2019-05-04 16:03:40 +0300

    Добавление базового тестового класса

--- addressbook-web-tests/addressbook-web-tests/ContactCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/ContactCreationTests.cs
@@ -3,48 +3,12 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
 using NUnit.Framework;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
 
 namespace WebAddressbookTests
 {
     [TestFixture]
-    public class ContactCreationTests
+    public class ContactCreationTests : TestBase
     {
-        private IWebDriver driver;
-        private StringBuilder verificationErrors;
-        private string baseURL;
-        private bool acceptNextAlert = true;
-        string loginField = "user";
-        string passwordField = "pass";
-        string firstnameField = "firstname";
-        string lastnameField = "lastname";
-        
-
-        [SetUp]
-        public void SetupTest()
-        {
-            driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
-            baseURL = "http://localhost:8080/";
-            verificationErrors = new StringBuilder();
-        }
-
-        [TearDown]
-        public void TeardownTest()
-        {
-            try
-            {
-                driver.Quit();
-            }
-            catch (Exception)
-            {
-                // Ignore errors if unable to close the browser
-            }
-            Assert.AreEqual("", verificationErrors.ToString());
-        }
-
         [Test]
         public void ContactCreationTest()
         {
@@ -56,100 +20,6 @@ namespace WebAddressbookTests
             Submit();
             ReturnToHomePage();
             Logout();
-        }
-
-        private void Logout()
-        {
-            driver.FindElement(By.LinkText("Logout")).Click();
-        }
-
-        private void ReturnToHomePage()
-        {
-            driver.FindElement(By.LinkText("home page")).Click();
-        }
-
-        private void Submit()
-        {
-            driver.FindElement(By.Name("submit")).Click();
-        }
-
-        private void FillContactForm(ContactData contact)
-        {
-            driver.FindElement(By.Name(firstnameField)).Click();
-            driver.FindElement(By.Name(firstnameField)).Clear();
-            driver.FindElement(By.Name(firstnameField)).SendKeys(contact.Firstname);
-            driver.FindElement(By.Name(lastnameField)).Click();
-            driver.FindElement(By.Name(lastnameField)).Clear();
-            driver.FindElement(By.Name(lastnameField)).SendKeys(contact.Lastname);
-        }
-
-        private void InitContactCreation()
-        {
-            driver.FindElement(By.LinkText("add new")).Click();
-        }
-
-        private void Login(AccountData account)
-        {
-            driver.FindElement(By.Name(loginField)).Click();
-            driver.FindElement(By.Name(loginField)).Clear();
-            driver.FindElement(By.Name(loginField)).SendKeys(account.Username);
-            driver.FindElement(By.Name(passwordField)).Click();
-            driver.FindElement(By.Name(passwordField)).Clear();
-            driver.FindElement(By.Name(passwordField)).SendKeys(account.Password);
-            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
-        }
-
-        private void OpenHomePage()
-        {
-            driver.Navigate().GoToUrl(baseURL + "addressbook/");
-        }
-
-        private bool IsElementPresent(By by)
-        {
-            try
-            {
-                driver.FindElement(by);
-                return true;
-            }
-            catch (NoSuchElementException)
-            {
-                return false;
-            }
-        }
-
-        private bool IsAlertPresent()
-        {
-            try
-            {
-                driver.SwitchTo().Alert();
-                return true;
-            }
-            catch (NoAlertPresentException)
-            {
-                return false;
-            }
-        }
-
-        private string CloseAlertAndGetItsText()
-        {
-            try
-            {
-                IAlert alert = driver.SwitchTo().Alert();
-                string alertText = alert.Text;
-                if (acceptNextAlert)
-                {
-                    alert.Accept();
-                }
-                else
-                {
-                    alert.Dismiss();
-                }
-                return alertText;
-            }
-            finally
-            {
-                acceptNextAlert = true;
-            }
         }
     }
 }
--- addressbook-web-tests/addressbook-web-tests/GroupRemovalTests.cs
+++ addressbook-web-tests/addressbook-web-tests/GroupRemovalTests.cs
@@ -10,23 +10,19 @@ using OpenQA.Selenium.Support.UI;
 namespace WebAddressbookTests
 {
     [TestFixture]
-    public class GroupCreationTests : TestBase
+    public class GroupRemovalTests : TestBase
     {
-
         [Test]
-        public void GroupCreationTest()
+        public void GroupRemovalTest()
         {
             OpenHomePage();
             Login(new AccountData("admin", "secret"));
             GoToGroupsPage();
-            InitGroupCreation();
-            GroupData group = new GroupData("aaa");
-            group.Header = "bbb";
-            group.Footer = "ccc";
-            FillGroupForm(group);
-            Submit();
+            SelectGroup(1);
+            DeleteGroup();
             ReturnToGroupsPage();
             Logout();
         }
+
     }
 }
