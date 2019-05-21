4d71ddb30b21ae436b7b742af5ceefed5a857e80
Fedotova-EE <Fedotova-EE@gaz-is.ru>
2019-05-21 08:07:19 +0300

    добавила тест с добавлением пустого контакта

--- addressbook-web-tests/addressbook-web-tests/Tests/ContactCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/Tests/ContactCreationTests.cs
@@ -29,6 +29,27 @@ namespace WebAddressbookTests
             newContacts.Sort();
             Assert.AreEqual(oldContacts.Count, newContacts.Count);
         }
+
+        [Test]
+        public void EmrtyContactCreationTest()
+        {
+            //preporation
+            ContactData contact = new ContactData("", "");
+            List<ContactData> oldContacts = app.Contacts.GetContactList();
+
+            //action
+            app.Contacts.Create(contact);
+
+            //verivication
+            Assert.AreEqual(oldContacts.Count + 1, app.Contacts.GetContactCount());
+
+            List<ContactData> newContacts = app.Contacts.GetContactList();
+            oldContacts.Add(contact);
+            oldContacts.Sort();
+            newContacts.Sort();
+            Assert.AreEqual(oldContacts.Count, newContacts.Count);
+        }
+
     }
 }
 
